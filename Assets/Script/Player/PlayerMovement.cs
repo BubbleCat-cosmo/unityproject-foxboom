eacbdcbfcd55ad8f3ced970e87f7fbcf1ff0b1ba
BubbleCat <1814472617@qq.com>
2025-11-25 15:41:34 +0800

    refactor:优化了角色的移动，在Update中获取玩家输入，FixedUpdate中进行移动操作

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -7,6 +8,8 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Animator animator;
 
+    private float playerInputX;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -15,6 +18,9 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        // 获取移动输入
+        playerInputX = Input.GetAxis("Horizontal");
+        playerInputX = Mathf.Min(playerInputX, 1);
         // 处理待机-移动切换动画
         HandleAnimation();
     }
@@ -28,26 +34,20 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move()
     {
-        // 获取Axis值
-        float inputX = Input.GetAxis("Horizontal");
-        // float inputY = Input.GetAxis("Vertical");
-
-        // 通过改变速度实现位置变换
-        Vector2 velocity = new Vector2(inputX * moveSpeed, 0);
+        float curVelocityY = rb.velocity.y;
+        Vector2 velocity = new Vector2(playerInputX * moveSpeed, curVelocityY);
         rb.velocity = velocity;
-
     }
     private void HandleAnimation()
     {
-        // 获取Axis值
-        float inputX = Input.GetAxis("Horizontal");
+
         // 切换动画
-        bool isMove = inputX != 0;
+        bool isMove = playerInputX != 0;
         animator.SetBool("Move", isMove);
         // 转向
-        if (inputX > 0)
+        if (playerInputX > 0)
             transform.localScale = new Vector3(4, 4, 1);
-        else if (inputX < 0)
+        else if (playerInputX < 0)
             transform.localScale = new Vector3(-4, 4, 1);
     }
 }
