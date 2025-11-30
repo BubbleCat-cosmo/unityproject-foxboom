2f6e0b0a4fc38cf29d61855fdabf04aa36246b34
BubbleCat <1814472617@qq.com>
2025-11-30 23:07:38 +0800

    feat:新增相机跟随角色移动的脚本和Pixel Perfect Camera组件，避免角色移动时瓦片地图出现缝隙

---
+++ Assets/Script/Camera/CameraController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    private void LateUpdate()
+    {
+        Vector3 pos = target.position;
+        pos.z = -10;
+        transform.position = pos;
+
+    }
+}
